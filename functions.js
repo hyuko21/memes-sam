2a5f69962136c7d6f358a536938649c786fcb7ea
leao3 <jose@ubuntu.ubuntu-domain>
2018-07-31 21:15:04 -0300

    adicionando o front até o momento

---
+++ functions.js
@@ -0,0 +1,18 @@
+
+
+function vinDisel(){
+    var imagem= document.getElementById("imagememegerada")
+    imagem.src='./imagens/download.jpeg'
+}
+
+
+function CaradoPanico(){
+    var imagem= document.getElementById("imagememegerada")
+    imagem.src='./imagens/maxresdefault (1).jpg'
+}
+
+
+function Irineu(){
+    var imagem= document.getElementById("imagememegerada")
+    imagem.src='./imagens/maxresdefault.jpg'
+}
